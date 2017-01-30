87e3728536793f9b75603e55d5c0beaf995b12ce
Rocketpoodle <apg3076@rit.edu>
2017-01-30 14:40:34 -0500

    created curvefit finder

--- stringtest.py
+++ stringtest.py
@@ -2,34 +2,19 @@ __author__ = 'Austin'
 from DataSet import *
 import numpy as np
 from Polynomial import *
+import random
+
 
 data1 = DataSet(["x","y"])
-data1.appendDataPoint([1,2])
-data1.appendDataPoint([6,3],["y","x"])
-data1.insertDataPoint([2,4],1)
-print(data1)
-mat2 = data1.toMatrix(vars = "y")
-mat1 = []
-col = []
-for x in range(0, data1.lenData):
-    col.append(1)
-mat1.append(col)
-mat1.append(data1.data[0])
-mat1 = np.matrix(mat1)
-mat3 = mat1.transpose()
-print(mat1)
-print(mat2)
-mat2 = mat1*mat2
-mat1 = mat1*mat3
-print(mat1)
-print(mat2)
-solution = np.linalg.qr(mat1)
-print(solution)
-done = np.linalg.solve(mat1,mat2)
-print(done)
-coeffs = []
-for x in range(0, 2):
-    coeffs.append(done.item(x))
-coeffs.reverse()
-poly = Polynomial(coeffs)
-print(poly)
+end = random.randrange(2,8)
+end2 = random.randrange(2,8)
+for x in range (0,end):
+    eval = random.randrange(-10,10)
+    for y in range(0,end2):
+        eval2 = random.randrange(-2,2)
+        data1.appendDataPoint([((x*end2)+y),eval+eval2])
+fit,rsq = data1.curveFit("y","x")
+print(fit, rsq)
+polyData = fit.evaluate(data1.getDataVariable("x"))
+data1.addDataVariable("fit", polyData)
+data1.plotData("x", ["y","fit"])
